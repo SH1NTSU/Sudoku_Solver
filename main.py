f1ba0896f0c31e30e0fabc1abd75a05c104412bc
Marcel Budziszewski <u84_marbud_waw@technischools.com>
2024-04-09 23:40:01 +0200

    funkcje pomocnicze

--- main.py
+++ main.py
@@ -2,6 +2,100 @@ class Sudoku:
     def __init__(self, board) -> None:
        self.board = board
     
+    def extract_zones(self,board):
+        """Funkcja służąca do przeczytania i wyjęcia 
+        liczb z każdej linijki, kolumny, i kwadratu 3x3 (zone)
+        nr_elements to znaczy elementy zapełnione nie zera
+        """
+        zones = []
+        # linijki
+        for row in  range(0, len(board)):
+            zone = {}
+            zone["type"] = "row"
+            zone["len"] = len(board[row]) - board[row].count(0)
+            zone["position"] = row
+            self.insert_sorted(zone, zones)
+        # kolumny
+        for col in range(0,9):
+            nr_elements = 0
+            for row in range(0,9):
+                if board[row][col] != 0:
+                    nr_elements += 1
+            zone = {}
+            zone["type"] = "col"
+            zone["len"] = nr_elements
+            zone["position"] = col
+            self.insert_sorted(zone, zones)
+        # kwadraty
+        for square in self.generate_square_positions():
+            nr_elements
+            for row in range(square["row_start"], square["row_end"]+1):
+                for col in range(square["col_start"], square["col_end"]+1):
+                    if board[row][col] != 0:
+                        nr_elements += 1
+            zone = {}
+            zone["type"] = "square"
+            zone["len"] = nr_elements
+            zone["position"] = tuple(square.values)
+            self.insert_sorted(zone, zones)
+
+        return zones
+    
+    def generate_square_positions(self):
+        """Ta funkcja służy budowie krotki słowników z pozycjami kwadratów"""
+        square_positions = []
+        row_start = 0
+        row_end = 2
+        col_start = 0
+        col_end = 2
+
+        while len(square_positions) < 9:
+            square_positions.append({
+                "row_start" : row_start,
+                "row_end" : row_end,
+                "col_start" : col_start,
+                "col_end" : col_end
+            })
+            if col_start < 6 and col_end < 8:
+                col_start += 3
+                col_end += 3
+            else:
+                col_start = 0
+                col_end = 2
+                row_start += 3
+                row_end += 3
+    def insert_sorted(zone, zones):
+        """Ta funkcja bierze słownik określający 
+        daną strefe i wrzuca ją do listy straf po długość pola. 
+        Robie to po to żeby było posortowane i 
+        żeby zaczynało od tych stref z większą ilośćą pól. 
+        To usprawni kod w prędkość rozwiązywania """
+        if len(zones) == 0:
+            zones.append(zone)
+        else:
+            inserted = False
+            for i in range(0, len(zones)):
+                if zone["len"] > zones [i]["len"]:
+                    zones.insert(i, zone)
+                    inserted = True
+                    break
+                else:
+                    continue
+            if not inserted:
+                zones.append(zone)
+                
+
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     def solve(self):
         pass
         
@@ -35,3 +129,4 @@ if __name__ == "__main__":
     s = Sudoku(puzzle)
     print('=' * 50)
     s.print_board()
+    print('=' * 50)
